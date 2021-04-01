83e6760b1aec5ac5b686effa5cf8e6e489fae716
komizutama <komizutama@gmail.com>
2021-04-01 11:34:25 -0400

    Added Search Bar boilerplat

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 
+import SearchBar from './SearchBar';
+
 class App extends React.Component{
     render() {
-        return <div> App</div>
+        return (
+        <div><SearchBar/></div>
+        );
     }
 }
 
