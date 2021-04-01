fe6dbcaac7e44fb20ab64fb2e5e7eaaf239dff66
komizutama <komizutama@gmail.com>
2021-04-01 11:30:25 -0400

    Initial App Boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+class App extends React.Component{
+    render() {
+        return <div> App</div>
+    }
+}
+
+export default App;
